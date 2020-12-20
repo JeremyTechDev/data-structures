d4baa76068d0c249408c9060629a8f11b4aba73f
Jeremy Munoz Torres <jeremy2918@gmail.com>
2020-12-19 22:01:52 -0400

    Added magic methods to all data structures

---
+++ Heap/tests.py
@@ -0,0 +1,121 @@
+import unittest
+from heap import Heap
+
+
+class TestMinHeap(unittest.TestCase):
+    def test_init(self):
+        heap = Heap()
+        self.assertEqual(heap.size(), 0)
+        self.assertEqual(heap.data, [])
+        heap = Heap(0)
+        self.assertEqual(heap.size(), 1)
+        self.assertEqual(heap.data, [0])
+        heap = Heap([1, 2, 3])
+        self.assertEqual(heap.size(), 3)
+        self.assertEqual(heap.data, [1, 2, 3])
+
+    def test_size(self):
+        heap = Heap()
+        self.assertEqual(heap.size(), 0)
+        heap.add(4)
+        heap.add(5)
+        heap.add(2)
+        self.assertEqual(heap.size(), 3)
+
+    def test_add(self):
+        heap = Heap()
+        heap.add(0)
+        self.assertEqual(heap.size(), 1)
+        self.assertEqual(heap.data, [0])
+        heap.add(1)
+        self.assertEqual(heap.size(), 2)
+        self.assertEqual(heap.data, [0, 1])
+        heap.add(-1)
+        self.assertEqual(heap.size(), 3)
+        self.assertEqual(heap.data, [-1, 1, 0])
+
+    def test_poll(self):
+        heap = Heap()
+        heap.add(0)
+        self.assertEqual(heap.poll(), 0)
+        self.assertTrue(heap.is_empty())
+        heap.add(0)
+        heap.add(5)
+        heap.add(-1)
+        self.assertEqual(heap.poll(), -1)
+        self.assertEqual(heap.data, [0, 5])
+
+    def test_remove(self):
+        heap = Heap()
+        self.assertRaises(Exception, heap.remove, 0)
+        heap.add(0)
+        self.assertRaises(Exception, heap.remove, 1)
+        heap.add(1)
+        heap.add(-1)
+        heap.remove(0)
+        self.assertEqual(heap.data, [-1, 1])
+        heap.remove(-1)
+        self.assertEqual(heap.data, [1])
+        heap.remove(1)
+        self.assertEqual(heap.data, [])
+
+    def test_swim(self):
+        heap = Heap()
+        heap.data = [0, 1, -1]
+        heap.swim(2)
+        self.assertEqual(heap.data, [-1, 1, 0])
+
+        heap.data = [-1, 1, 0, -5]
+        heap.swim(3)
+        self.assertEqual(heap.data, [-5, -1, 0, 1])
+
+    def test_sink(self):
+        heap = Heap()
+        heap.data = [5, 0, 1, -1]
+        heap.sink(0)
+        self.assertEqual(heap.data, [0, -1, 1, 5])
+        heap.data = [0, 10, 5, 2, 2]
+        heap.sink(1)
+        self.assertEqual(heap.data, [0, 2, 5, 10, 2])
+        heap.sink(2)
+        self.assertEqual(heap.data, [0, 2, 5, 10, 2])
+
+    def test_swap(self):
+        heap = Heap()
+        heap.data = [5, 2]
+        heap.swap(0, 1)
+        self.assertEqual(heap.data, [2, 5])
+
+    def test_is_empty(self):
+        heap = Heap()
+        self.assertTrue(heap.is_empty())
+        heap.add(0)
+        self.assertFalse(heap.is_empty())
+
+    def test_peek(self):
+        heap = Heap()
+        self.assertEqual(heap.peek(), None)
+        heap.add(0)
+        self.assertEqual(heap.peek(), 0)
+        heap.add(-1)
+        self.assertEqual(heap.peek(), -1)
+
+    def test_index(self):
+        heap = Heap()
+        self.assertEqual(heap.index(0), -1)
+        heap.add(0)
+        self.assertEqual(heap.index(0), 0)
+        heap.add(-1)
+        heap.add(1)
+        self.assertEqual(heap.index(-1), 0)
+
+    def test_contains(self):
+        heap = Heap()
+        self.assertFalse(heap.contains(0))
+        heap.add(0)
+        self.assertTrue(heap.contains(0))
+        self.assertFalse(heap.contains(1))
+
+
+if __name__ == '__main__':
+    unittest.main()
